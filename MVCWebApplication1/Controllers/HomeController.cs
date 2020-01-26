f74f4860b48b5b625585d23fd70e20f4ab693bf7
Ashutosh Jha <ashutosh.jha@vanderlande.com>
2020-01-27 00:55:42 +0530

    First MVC commit

---
+++ MVCWebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCWebApplication1.Models;
+
+namespace MVCWebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee(){EmployeeId =101,Name="Ashutosh", Salary=500000,Department="IT"},
+                new Employee(){EmployeeId =102,Name="Alok", Salary=500000,Department="Army"},
+                new Employee(){EmployeeId =103,Name="Amit", Salary=500000,Department="CS"},
+                new Employee(){EmployeeId =104,Name="Varun", Salary=500000,Department="EC"},
+                new Employee(){EmployeeId =105,Name="Pravin", Salary=500000,Department="Test"}
+            };
+            //ViewBag.Countries = new List<string>()
+            //{
+            //    "India",
+            //    "US",
+            //    "China",
+            //    "Nepal"
+            //};
+            return View(employees);
+        }
+        public string GetDetails()
+        {
+            return "Hello From GetDetails method...";
+        }
+    }
+}
---
+++ MVCWebApplication1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MVCWebApplication1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Vanderlande")]
+[assembly: AssemblyProduct("MVCWebApplication1")]
+[assembly: AssemblyCopyright("Copyright © Vanderlande 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bf6e7619-3d2d-4ae3-92c7-37d0b8629bed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
